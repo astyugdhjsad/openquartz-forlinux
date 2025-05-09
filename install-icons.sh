2662ed8de87bd4410de8a66ac51bc2f8c59aa0d6
astyugdhjsad <101475731+astyugdhjsad@users.noreply.github.com>
2025-05-09 01:08:26 +0100

    Create install-icons.sh

---
+++ install-icons.sh
@@ -0,0 +1,4 @@
+# assumes you have installed openbox and x11 prior to icon installation
+curl -O
+sudo mkdir -p /usr/share/icons/osx108/
+sudo mkdir -p /usr/share/icons/osx108 {16x16,32x32,48x48,64x64,128x128}
