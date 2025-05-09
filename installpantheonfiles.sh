33882ef9a75d39b436494dfeada3beea550e6c69
astyugdhjsad <101475731+astyugdhjsad@users.noreply.github.com>
2025-05-09 21:43:08 +0100

    Update installpantheonfiles.sh

--- installpantheonfiles.sh
+++ installpantheonfiles.sh
@@ -13,7 +13,11 @@ read -p "Press Enter to continue..."
 # Install dependencies
 sudo apt install meson valac libcanberra-dev libcloudproviders-dev libdbus-glib-1-dev libgee-0.8-dev libgit2-glib-1.0-dev libglib2.0-dev libgranite-dev libgtk-3-dev libhandy-1-dev libpango1.0-dev libportal-dev libportal-gtk3-dev libsqlite3-dev libunity-dev libzeitgeist-2.0-dev
 
+# Clone repo
+git clone https://github.com/elementary/files
+
 # Build
+cd files
 meson build --prefix=/usr
 cd build
 ninja
